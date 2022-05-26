0af1638e31d91aa236917b872272fc1e7009e410
Hai Do Minh <hai.do@ntq-solution.com.vn>
2022-05-26 14:47:30 +0700

    update

--- src/common/config.js
+++ src/common/config.js
@@ -1,7 +1,6 @@
 module.exports = {
   baseUrl: {
-      // test: 'http://ec2-52-194-64-247.ap-northeast-1.compute.amazonaws.com'
-      test: 'https://f50f-117-4-192-4.ngrok.io'
+    test: 'https://support-tool.ntq.solutions'
   },
   autoUpdateTime: '02:00'
 }
